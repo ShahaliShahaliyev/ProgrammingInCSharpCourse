142853d663f566b5bfdea64a5f5ff50b16f56dde
sahali <sahalisahaliyev@9gmail.com>
2021-11-24 19:17:55 +0400

    Tasks added!

---
+++ Lesson12/Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Shahali\Downloads\Lesson12";
+
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+            foreach (var directory in directories)
+            {
+                Directory.Delete(directory);
+            }
+            foreach (var file in files)
+            {
+                switch (Path.GetExtension(file))
+                {
+                    case ".txt":
+                        File.Move(file, @"C:\Users\Shahali\Downloads\Lesson12\text", true);
+                        break;
+                    case ".mp4":
+                        File.Move(file, @"C:\Users\Shahali\Downloads\Lesson12\video", true);
+                        break;
+                    case ".mp3":
+                        File.Move(file, @"C:\Users\Shahali\Downloads\Lesson12\music", true);
+                        break;
+                    default:
+                        File.Move(file, @"C:\Users\Shahali\Downloads\Lesson12\other", true);
+                        break;
+
+                }
+            }
+        }
+    }
+}
