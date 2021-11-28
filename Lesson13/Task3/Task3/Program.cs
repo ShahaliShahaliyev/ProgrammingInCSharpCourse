4a9fe1b0c7af08b1f35a7820ca26a4bafb7f36df
sahali <sahalisahaliyev@9gmail.com>
2021-11-28 14:01:28 +0400

    Tasks added!

---
+++ Lesson13/Task3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+            var value = cpuCounter.NextValue();
+            
+            if (Math.Abs(value) <= 0.00)
+                value = cpuCounter.NextValue();
+
+            Console.WriteLine($"CPU: {value}%");
+        }
+
+        
+    }
+}
