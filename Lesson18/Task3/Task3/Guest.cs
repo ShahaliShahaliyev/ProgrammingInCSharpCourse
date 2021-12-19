26f3165a99a3597ef66fae460a08fb80a1a6bd89
sahali <sahalisahaliyev@9gmail.com>
2021-12-19 17:42:17 +0400

    Tasks added!

---
+++ Lesson18/Task3/Task3/Guest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Guest
+    {
+        public string Name { get; set; }
+    }
+
+    public class Table
+    {
+        public Table()
+        {
+            Guests = new List<Guest>()
+            {
+             new Guest(){Name ="John"},
+             new Guest(){Name = "Charlie"},
+             new Guest() {Name = "Jill"},
+             new Guest(){Name = "Martin"},
+             new Guest(){Name = "Ann"},
+             new Guest(){Name = "Eve"}
+            };
+        }
+
+        public List<Guest> Guests { get; set;}
+
+        public Guest this[int indexer]
+        {
+            get=> Guests[indexer];
+            set => Guests[indexer] = value;
+        }
+
+        public Guest this[int indexer,string name]
+        {
+            get => Guests.Where(guest => guest.Name == name).FirstOrDefault();
+            set => Guests[indexer] =value;
+        }
+
+    }
+}
---
+++ Lesson18/Task3/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task3
+{
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+               Table list = new Table();
+            Console.WriteLine(list[0].Name);
+            Console.WriteLine(list[1,"Charlie"].Name);
+            Console.WriteLine(list[2].Name);
+            Console.WriteLine(list[3, "Martin"].Name);
+            Console.WriteLine(list[4].Name);
+            Console.WriteLine(list[5,"Eve"].Name);
+        }
+        }
+}
