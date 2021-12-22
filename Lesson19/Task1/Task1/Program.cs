b19100cb2bfec0a678824f40d99ec933876b5e1d
sahali <sahalisahaliyev@9gmail.com>
2021-12-22 16:12:53 +0400

    Tasks added!

---
+++ Lesson19/Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread method1 = new Thread(() => Method1(12));
+            method1.Start();
+
+            Thread method2 = new Thread(() => Method2(12));
+           method2.Start();
+
+        static void Method1(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Method1-{i}");
+                Thread.Sleep(500);
+            }
+
+        }
+        static void Method2(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Method2-{i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Lesson19/Task2/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+       
+        static async Task Main(string[] args)
+        {
+            const string url = "https://catfact.ninja/fact";
+
+            HttpClient httpClient = new HttpClient();
+
+            bool isContinue;
+
+            do
+            {
+                var result = await httpClient.GetStringAsync(url);
+                var properties = JsonConvert.DeserializeObject<Properties>(result);
+
+                Console.WriteLine(properties.Fact +"\n"+ properties.Length);
+
+                Console.WriteLine("*");
+                Console.WriteLine("to continue:enter true \n to stop:enter false");
+
+                isContinue = Convert.ToBoolean(Console.ReadLine());
+
+
+            } while (isContinue);
+
+
+
+
+        }
+    }
+}
