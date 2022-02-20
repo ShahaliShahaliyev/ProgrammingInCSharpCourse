a30fe91f83c0606227acb660a954c720a83477f2
sahali <sahalisahaliyev@9gmail.com>
2022-02-20 14:01:41 +0400

    Tasks Added!

---
+++ Lesson32  web/Lesson32  web/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Lesson32__web.Models;
+using System.Linq;
+
+namespace Lesson32__web.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class EmployeeController : Controller
+    {
+        private static List<Employees> _employees = new List<Employees>();
+        
+        [HttpGet("all")]
+        public List<Employees> GetAll()
+        {
+            return _employees;
+        }
+
+        [HttpPost("create")]
+        public Employees CreateEmployee(Employees employees)
+        {
+            _employees.Add(employees);
+            return employees;
+        }
+
+        [HttpPut("update")]
+        public Employees UpdateEmployees(int id,Employees newEmployees)
+        {
+            var employee = _employees.FirstOrDefault(x => x.Id == id);
+            employee.Name = newEmployees.Name;
+            return newEmployees;
+        }
+
+        [HttpDelete("delete")]
+        public Employees DeleteEmployees(int id)
+        {
+            var employees = _employees.FirstOrDefault(x => x.Id == id);
+            _employees.Remove(employees);
+            return employees;
+        }
+    }
+}
---
+++ Lesson32  web/Lesson32  web/Models/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson32__web.Models
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+        public int Salary { get; set; }
+        public bool IsManager { get; set; }
+    }
+}
