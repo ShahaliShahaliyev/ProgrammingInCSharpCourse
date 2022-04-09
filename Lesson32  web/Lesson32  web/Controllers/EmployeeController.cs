6569f27cf6b8e9d8b947e4c1ee2ee8c4744e021c
sahali <sahalisahaliyev@9gmail.com>
2022-04-09 17:24:34 +0400

    Lesson Added!

--- Lesson32  web/Lesson32  web/Controllers/EmployeeController.cs
+++ Lesson32  web/Lesson32  web/Controllers/EmployeeController.cs
@@ -3,24 +3,40 @@ using System;
 using System.Collections.Generic;
 using Lesson32__web.Models;
 using System.Linq;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Logging;
 
 namespace Lesson32__web.Controllers
 {
+
     [ApiController]
     [Route("api/controller")]
     public class EmployeeController : Controller
     {
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(
+            ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
         private static List<Employees> _employees = new List<Employees>();
         
         [HttpGet("all")]
         public List<Employees> GetAll()
         {
+            _logger.LogInformation("Request accepted {date}",DateTime.Now);
+
             return _employees;
         }
 
         [HttpPost("create")]
         public Employees CreateEmployee(Employees employees)
         {
+            _logger.LogInformation("Posted {date}",DateTime.Now);
+
             _employees.Add(employees);
             return employees;
         }
@@ -28,6 +44,10 @@ namespace Lesson32__web.Controllers
         [HttpPut("update")]
         public Employees UpdateEmployees(int id,Employees newEmployees)
         {
+
+            _logger.LogInformation("Updated{date}", DateTime.Now);
+
+
             var employee = _employees.FirstOrDefault(x => x.Id == id);
             employee.Name = newEmployees.Name;
             return newEmployees;
@@ -36,6 +56,10 @@ namespace Lesson32__web.Controllers
         [HttpDelete("delete")]
         public Employees DeleteEmployees(int id)
         {
+
+            _logger.LogInformation("Deleted {date}", DateTime.Now);
+
+
             var employees = _employees.FirstOrDefault(x => x.Id == id);
             _employees.Remove(employees);
             return employees;
--- Lesson32  web/Lesson32  web/Program.cs
+++ Lesson32  web/Lesson32  web/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace Lesson32__web
 {
@@ -13,11 +15,18 @@ namespace Lesson32__web
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext().WriteTo.File(
+                    System.IO.Path.Combine("D:\\LogFiles", "Application", "logs.text"))
+                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
+                
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
